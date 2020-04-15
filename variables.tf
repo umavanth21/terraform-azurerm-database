4e4d2c254d93a277607fd816606c11f892569140
umavanth21 <56533761+umavanth21@users.noreply.github.com>
2020-04-15 19:20:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+variable "subscription_id" {
+  }
+variable "client_id" {
+  }
+variable "tenant_id" {
+  }
+variable "client_secret" {
+  }
 variable "resource_group_name" {
   description = "Default resource group name that the database will be created in."
   default     = "TARGDND"
